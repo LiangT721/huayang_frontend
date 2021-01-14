b96b96c2de15f14bf16b001bb9a169e5e59bbd18
Liang <taylor721@gmail.com>
2021-01-13 18:22:48 -0700

    create the initial site

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,15 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+    state: {
+        language: "Eng"
+    },
+    mutations: {},
+    actions: {},
+    modules: {},
+    getters: {
+        lan: state => {
+            return state.language;
+        }
+    }
 });
